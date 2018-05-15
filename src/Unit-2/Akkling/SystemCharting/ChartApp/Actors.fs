61d7676fba91cd90745ea30482364dbd1be1a777
Kleidemos <19260292+kleidemos@users.noreply.github.com>
2018-05-15 22:18:37 +0500

    Добавил проект для Akkling Unit-2.

---
+++ src/Unit-2/Akkling/SystemCharting/ChartApp/Actors.fs
@@ -0,0 +1,23 @@
+﻿namespace ChartApp
+
+open System.Collections.Generic
+open System.Windows.Forms.DataVisualization.Charting
+open Akka.Actor
+open Akkling
+
+[<AutoOpen>]
+module Messages =
+    type InitializeChart =
+        | InitializeChart of initialSeries: Map<string, Series>
+
+/// Actors used to intialize chart data
+module ChartingActor =
+    let create (chart : Chart) =
+        actorOf <| function
+            | InitializeChart series ->
+                chart.Series.Clear()
+                series |> Map.iter (fun k v ->
+                    v.Name <- k
+                    chart.Series.Add v)
+                ignored()
+        |> props
---
+++ src/Unit-2/Akkling/SystemCharting/ChartApp/Program.fs
@@ -0,0 +1,19 @@
+﻿module Program
+
+open System
+open System.Windows.Forms
+open Akkling
+open Akka.Actor
+open Akka.Configuration.Hocon
+open System.Configuration
+open ChartApp
+
+let chartActors =
+    Configuration.load ()
+    |> System.create "ChartActors"
+
+Application.EnableVisualStyles ()
+Application.SetCompatibleTextRenderingDefault false
+
+[<STAThread>]
+do Application.Run (Form.load chartActors)
