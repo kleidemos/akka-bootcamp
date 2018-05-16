85a0f0e76900daa1c8bb05dde3e750b4ff1a30de
Kleidemos <19260292+kleidemos@users.noreply.github.com>
2018-05-16 08:52:07 +0500

    Сделал урок 2.3.
    * Однако в голове отнюдь не все сохранилось.

--- src/Unit-2/Akkling/SystemCharting/ChartApp/Actors.fs
+++ src/Unit-2/Akkling/SystemCharting/ChartApp/Actors.fs
@@ -8,26 +8,192 @@ open Akkling
 [<AutoOpen>]
 module Messages =
     type ChartMessage =
-        | InitializeChart of initialSeries: Map<string, Series>
-        | AddSeries of series: Series
+        | InitializeChart of initialSeries : Map<string, Series>
+        | AddSeries of series : Series
+        | RemoveSeries of seriesName : string
+        | Metric of series : string * counterValue : float
+
+    let (|ChartMessage|_|) = tryUnbox<ChartMessage>
+
+    type CounterType =
+        | Cpu = 1
+        | Memory = 2
+        | Disk = 3
+
+    type CounterMessage =
+        | GatherMetrics
+        | SubscribeCounter of subscriber : IActorRef
+        | UnsubscribeCounter of subscriber : IActorRef
+
+    let (|CounterMessage|_|) = tryUnbox<CounterMessage>
+
+    type CoordinationMessage =
+        | Watch of counter : CounterType
+        | Unwatch of counter : CounterType
+
+    type ButtonMessage = Toggle
 
 /// Actors used to intialize chart data
 module ChartingActor =
     let create (chart : Chart) =
-        actorOf2 <| fun context ->
-            let rec behaviour mapping = function
+        props <| fun context ->
+            let maxPoints = 250
+
+            let setChartBoundaries mapping numberOfPoints =
+                let allPoints =
+                    mapping
+                    |> Map.toSeq
+                    |> Seq.collect (fun (_, series : Series) -> series.Points)
+                    |> HashSet<DataPoint>
+                if allPoints.Count > 2 then
+                    let yValues = allPoints |> Seq.collect (fun p -> p.YValues) |> Seq.toList
+                    let area = chart.ChartAreas.[0]
+                    area.AxisX.Maximum <- float numberOfPoints
+                    area.AxisX.Minimum <- float (numberOfPoints - maxPoints)
+                    area.AxisY.Maximum <- yValues |> List.fold max 1. |> System.Math.Ceiling
+                    area.AxisY.Minimum <- yValues |> List.fold min 0. |> System.Math.Floor
+
+            let (|SeriesName|_|) = function
+                | InitializeChart _ -> None
+                | AddSeries series -> Some series.Name
+                | RemoveSeries seriesName -> Some seriesName
+                | Metric (seriesName, _) -> Some seriesName
+
+            let rec behaviour mapping numberOfPoints = function
                 | InitializeChart series ->
                     chart.Series.Clear()
+                    chart.ChartAreas.[0].AxisX.IntervalType <- DateTimeIntervalType.Number
+                    chart.ChartAreas.[0].AxisY.IntervalType <- DateTimeIntervalType.Number
                     series |> Map.iter (fun k v ->
                         v.Name <- k
                         chart.Series.Add v)
                     ignored()
+                | SeriesName name
+                    when System.String.IsNullOrEmpty name ->
+                        unhandled()
                 | AddSeries series when
-                    not <| System.String.IsNullOrEmpty series.Name
-                    && not <| (mapping |> Map.containsKey series.Name) ->
-                    let mapping = mapping.Add (series.Name, series)
-                    chart.Series.Add series
-                    become (behaviour mapping)
-                | _ -> ignored()
-            behaviour Map.empty
-        |> props
+                    not <| (mapping |> Map.containsKey series.Name) ->
+                        let mapping = mapping.Add (series.Name, series)
+                        chart.Series.Add series
+                        setChartAndBecomeBehaviour mapping numberOfPoints
+                | RemoveSeries seriesName when
+                    mapping.ContainsKey seriesName ->
+                        chart.Series.Remove mapping.[seriesName] |> ignore
+                        let mapping = mapping.Remove seriesName
+                        setChartAndBecomeBehaviour mapping numberOfPoints
+                | Metric (seriesName, counterValue) when
+                    mapping.ContainsKey seriesName ->
+                        let numberOfPoints = numberOfPoints + 1
+                        let series = mapping.[seriesName]
+                        series.Points.AddXY (numberOfPoints, counterValue) |> ignore
+                        while (series.Points.Count > maxPoints) do series.Points.RemoveAt 0
+                        setChartAndBecomeBehaviour mapping numberOfPoints
+                | _ -> unhandled()
+            and setChartAndBecomeBehaviour mapping numberOfPoints =
+                setChartBoundaries mapping numberOfPoints
+                behaviour mapping numberOfPoints |> become
+
+            behaviour Map.empty 0
+            |> become
+
+module PerformanceCounterActor =
+    open System.Diagnostics
+
+    let create seriesName perfCounterGenerator =
+        props <| fun context ->
+            let counter : PerformanceCounter = perfCounterGenerator()
+            let cancelled =
+                context.ScheduleRepeatedly
+                    (System.TimeSpan.FromMilliseconds 250.)
+                    (System.TimeSpan.FromMilliseconds 250.)
+                    context.Self
+                    GatherMetrics
+
+            let rec behaviour subscriptions = function
+                | LifecycleEvent PostStop ->
+                    cancelled.Cancel()
+                    counter.Dispose()
+                    ignored()
+                | CounterMessage GatherMetrics ->
+                    subscriptions |> Seq.iter (
+                        let msg = Metric(seriesName, counter.NextValue() |> float)
+                        fun p -> typed p <! msg)
+                    ignored()
+                | CounterMessage (SubscribeCounter sub) ->
+                    let subscriptionsWithoutSubscriber =
+                        subscriptions |> List.filter (fun actor -> actor <> sub)
+                    sub::subscriptionsWithoutSubscriber
+                    |> behaviour
+                    |> become
+                | CounterMessage (UnsubscribeCounter sub) ->
+                    subscriptions
+                    |> List.filter (fun actor -> actor <> sub)
+                    |> behaviour
+                    |> become
+                | _ -> unhandled()
+
+            behaviour [] |> become
+
+module PerformanceCounterCoordinatorActor =
+    open System.Diagnostics
+    open System.Drawing
+
+    let create (chartingActor : IActorRef<ChartMessage>) =
+        props <| fun context ->
+            let counterGenerators =
+                [   CounterType.Cpu, fun _ ->
+                        new PerformanceCounter("Processor", "% Processor Time", "_Total", true)
+                    CounterType.Memory, fun _ ->
+                        new PerformanceCounter("Memory", "% Committed Bytes In Use", true)
+                    CounterType.Disk, fun _ ->
+                        new PerformanceCounter("LogicalDisk", "% Disk Time", "_Total", true) ]
+                |> Map.ofList
+            let counterSeries =
+                [   CounterType.Cpu, SeriesChartType.SplineArea, Color.DarkGreen
+                    CounterType.Memory, SeriesChartType.FastLine, Color.MediumBlue
+                    CounterType.Disk, SeriesChartType.SplineArea, Color.DarkRed ]
+                |> List.map (fun (counterType, chartType, color) ->
+                    counterType, fun _ -> new Series(string counterType, ChartType = chartType, Color = color))
+                |> Map.ofList
+
+            let rec behaviour counterActors = function
+                | Watch counter when counterActors |> Map.containsKey counter |> not ->
+                    let counterName = string counter
+                    let actor =
+                        PerformanceCounterActor.create counterName counterGenerators.[counter]
+                        |> spawn context (sprintf "counterActor-%s" counterName)
+                    let counterActors = counterActors.Add (counter, actor)
+                    chartingActor <! AddSeries (counterSeries.[counter] ())
+                    actor <! SubscribeCounter (untyped chartingActor)
+                    behaviour counterActors |> become
+                | Watch counter ->
+                    chartingActor <! AddSeries (counterSeries.[counter] ())
+                    counterActors.[counter] <! SubscribeCounter (untyped chartingActor)
+                    ignored()
+                | Unwatch counter when counterActors |> Map.containsKey counter ->
+                    chartingActor <! RemoveSeries ((counterSeries.[counter] ()).Name)
+                    counterActors.[counter] <! UnsubscribeCounter (untyped chartingActor)
+                    ignored()
+                | _ -> unhandled()
+            become (behaviour Map.empty)
+
+module ButtonToggleActor =
+    let create coordinatorActor (button : System.Windows.Forms.Button) counterType isToggled  =
+        props <| fun context ->
+            let flipToggle isOn =
+                let isToggledOn = not isOn
+                button.Text <-
+                    if isToggledOn then "ON" else "OFF"
+                    |> sprintf "%s (%s)" (counterType.ToString().ToUpperInvariant())
+                isToggledOn
+            let rec behaviour isToggledOn = function
+                | Toggle when isToggledOn ->
+                    coordinatorActor <! Unwatch counterType
+                    behaviour (flipToggle isToggledOn) |> become
+                | Toggle ->
+                    coordinatorActor <! Watch counterType
+                    behaviour (flipToggle isToggledOn) |> become
+                | _ -> unhandled()
+
+            behaviour isToggled
+            |> become
--- src/Unit-2/Akkling/SystemCharting/ChartApp/From.fs
+++ src/Unit-2/Akkling/SystemCharting/ChartApp/From.fs
@@ -11,18 +11,20 @@ module Form =
     let form = new Form(Name = "Main", Visible = true, Text = "System Metrics", AutoScaleDimensions = SizeF(6.F, 13.F), AutoScaleMode = AutoScaleMode.Font, ClientSize = Size(684, 446))
     let chartArea1 = new ChartArea(Name = "ChartArea1")
     let legend1 = new Legend(Name = "Legend1")
-    let series1 = new Series(Name = "Series1", ChartArea = "ChartArea1", Legend = "Legend1")
-
-    // Add this line to create the 'Add Series' button
-    let btnAddSeries = new Button(Name = "btnAddSeries", Text = "Add Series", Location = Point(540, 300), Size = Size(100, 40), TabIndex = 1, UseVisualStyleBackColor = true)
+    // create the buttons
+    let btnCpu = new Button(Name = "btnCpu", Text = "CPU (ON)", Location = Point(560, 275), Size = Size(110, 40), TabIndex = 1, UseVisualStyleBackColor = true)
+    let btnMemory = new Button(Name = "btnMemory", Text = "MEMORY (OFF)", Location = Point(560, 320), Size = Size(110, 40), TabIndex = 2, UseVisualStyleBackColor = true)
+    let btnDisk = new Button(Name = "btnDisk", Text = "DISK (OFF)", Location = Point(560, 365), Size = Size(110, 40), TabIndex = 3, UseVisualStyleBackColor = true)
 
     sysChart.BeginInit ()
     form.SuspendLayout ()
     sysChart.ChartAreas.Add chartArea1
     sysChart.Legends.Add legend1
-    sysChart.Series.Add series1
 
-    form.Controls.Add btnAddSeries
+    // and add them to the form
+    form.Controls.Add btnCpu
+    form.Controls.Add btnMemory
+    form.Controls.Add btnDisk
 
     form.Controls.Add sysChart
     sysChart.EndInit ()
@@ -32,15 +34,27 @@ module Form =
         let chartActor =
             ChartingActor.create sysChart
             |> spawn myActorSystem "charting"
-        let series = ChartDataHelper.randomSeries None None "FakeSeries1"
-        chartActor <! InitializeChart(Map.ofList [(series.Name, series)])
+        let coordinatorActor =
+            chartActor
+            |> PerformanceCounterCoordinatorActor.create
+            |> spawn myActorSystem "counters"
+        let toggleActors =
+            [   CounterType.Cpu, btnCpu, "cpuCounter"
+                CounterType.Memory, btnMemory, "memoryCounter"
+                CounterType.Disk, btnDisk, "diskCounter" ]
+            |> List.map (fun (counterType, btn, name) ->
+                counterType,
+                    { ButtonToggleActor.create coordinatorActor btn counterType false with
+                        Dispatcher = Some "akka.actor.synchronized-dispatcher" }
+                    |> spawn myActorSystem name)
+            |> Map.ofList
 
-        btnAddSeries.Click.Add (fun _ ->
-            let newSeries =
-                sysChart.Series.Count + 1
-                |> sprintf "FakeSeries %i"
-                |> ChartDataHelper.randomSeries None None
-            chartActor <! AddSeries newSeries
-            )
+        toggleActors.[CounterType.Cpu] <! Toggle
+
+        [   CounterType.Cpu, btnCpu, "cpuCounter"
+            CounterType.Memory, btnMemory, "memoryCounter"
+            CounterType.Disk, btnDisk, "diskCounter" ]
+        |> List.iter (fun (counterType, btn, _) ->
+            btn.Click.Add (fun _ -> toggleActors.[counterType] <! Toggle))
 
         form
